619eba23573378d7a483d3c1a35257937770a2e4
Daniel Stone <stoned12@my.erau.edu>
2026-01-30 10:40:21 -0700

    Addition: Slider widget

--- ui/widgets.py
+++ ui/widgets.py
@@ -61,4 +61,43 @@ class Slider:
 
         self.track_color = track_color or theme.GRAY
         self.handle_color = handle_color or theme.WHITE
-        
+
+        # Callback for changes in value
+        self.on_change = on_change
+
+        self.dragging = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self._handle_rect().collidepoint(event.pos):
+                self.dragging = True
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            self.dragging = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN and self.dragging:
+            x = event.pos[0]
+            x = max(self.rect.left, min(self.rect.right))
+            ratio = (x - self.rect.left) / self.rect.width
+            new_value = self.min_value + ratio * (self.max_value - self.min_value)
+
+            if int(new_value) != int(self.value):
+                self.value = new_value
+                if self.on_change:
+                    self.on_change(self.value)
+
+    def draw(self, surface):
+        # Draw Track
+        pygame.draw.rect(surface, self.track_color, self.rect, border_radius=5)
+
+        # Draw Handle
+        handle_rect = self._handle_rect()
+        center = handle_rect.center
+        radius = handle_rect.width // 2
+        pygame.draw.circle(surface, self.handle_color, center, radius)
+
+    def _handle_rect(self):
+        ratio = (self.value - self.min_value) / (self.max_value - self.min_value)
+        handle_x = self.rect.left + ratio * self.rect.width
+        size = 30 # Diameter of handle
+        return pygame.Rect(handle_x - size // 2, self.rect.centery - size // 2, size, size)

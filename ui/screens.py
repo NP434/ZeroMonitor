18f699a22d904ba04c9580e3e6f55875a8662ac0
Daniel Stone <stoned12@my.erau.edu>
2026-01-30 09:07:06 -0700

    Edit: 24-hr clock functionality

--- ui/screens.py
+++ ui/screens.py
@@ -1,4 +1,5 @@
 import pygame
+import datetime
 import ui.theme as theme
 from ui.widgets import Button
 
@@ -48,25 +49,54 @@ class MainScreen(BaseScreen):
             text="Settings"
         )
 
+        self.clock_button = Button(
+            rect=(20, 20, 150, 40),
+            text="",
+            bg_color=None,
+            border_radius=0
+        )
+        self.use_24hr = False
+
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             if self.settings_button.is_clicked(event.pos):
                 self.app.change_screen("settings")
+            
+            if self.clock_button.is_clicked(event.pos):
+                self.use_24hr = not self.use_24hr
 
     def draw(self, surface):
         surface.fill(theme.BLACK)
+
+        # --- Top Bar Elements ---
+
+        # Time (topleft)
+        if self.use_24hr:
+            now = datetime.datetime.now().strftime("%H:%M")
+        else:
+            now = datetime.datetime.now().strftime("%I:%M %p")
+
+        time_text = theme.DEFAULT_FONT.render(now, True, theme.WHITE)
+        surface.blit(time_text, (20, 20))
+
+        # Title Centered Horizontally
+        title_text = theme.DEFAULT_FONT.render("Zero Monitor Dashboard", True, theme.WHITE)
+        title_rect = title_text.get_rect(
+            center=(self.app.width // 2, 50)
+        )
+        surface.blit(title_text, title_rect)
 
         # Draw Button
         self.settings_button.draw(surface)
 
         # Draw remaining contents on screen
-        pygame.draw.rect(surface, theme.GRAY, (50, 300, 924, 120))
+        pygame.draw.rect(surface, theme.GRAY, (50, 150, 924, 120))
         text = theme.DEFAULT_FONT.render(
-            "Zero Monitor Dashboard",
+            "System stats here...",
             True,
             theme.WHITE
         )
-        surface.blit(text, (60, 350))
+        surface.blit(text, (60, 200))
 
 class SettingsScreen(BaseScreen):
     def __init__(self, app):

6e8131c1647d06be78583f39e07ea89ebe4bf97a
Noah Perea <noahperea323@gmail.com>
2026-02-25 14:05:56 -0700

    Added OS info retrieval for linux only, to test currrent flow. Uploading cert_gen for setting up endpoint

--- Pairing/transfer.sh
+++ Pairing/transfer.sh
@@ -9,6 +9,7 @@ echo "Setting File Paths"
 SSH_KEY="$HOME/.ssh/id_rsa.pub"
 SERVER_APP="Pairing/endpoint.py"
 SERVER_URL="https://127.0.0.1:8443"
+DEVICE_LIST="device_list.json"
 
 if [ ! -f "$SSH_KEY" ]; then
   echo "[*] SSH key does not exits."
@@ -41,6 +42,7 @@ while [ "$ELAPSED" -lt "$TIMEOUT" ]; do
     if [ "$STATUS" = "false" ]; then
         echo "[*] Pairing key retrieved, shutting down Flask endpoint..."
         kill $FLASK_PID
+        End_reason="closed"
         break
     fi
     sleep 1
@@ -50,4 +52,15 @@ done
 if [ "$ELAPSED" -ge "$TIMEOUT" ]; then
     echo "[!] Timeout reached, shutting down Flask endpoint..."
     kill $FLASK_PID
+    End_reason="timeout"
 fi
+
+if ["$End_reason" = "closed"] then
+    echo "Enter UserName on target device"
+    read UN
+    echo "Enter HostName of target device"
+    read HN
+
+    OS_INFO=$(ssh -i $HOME/.ssh/id_rsa "$UN@$HN" 'cat /etc/os-release' )
+    echo "$OS_INFO"
+

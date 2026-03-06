419e04622b8b9f64daf61689d4e0ca32f05014c9
Noah Perea <noahperea323@gmail.com>
2026-03-06 12:48:01 -0700

    test control module

--- Pairing/pairing_control.py
+++ Pairing/pairing_control.py
@@ -12,20 +12,18 @@ class ControlPairing:
     """Framework for the pairng class that drives the pairing module"""
     def __init__(self, bus:EventBus):
         self.bus = bus
-        self.logger = logging.getlogger("pairing")
+        #self.logger = logging.getlogger("pairing")
         self.bus.subscribe("ADD_NODE", self.add_node)
     
-    def add_node(self, d_name:str):
+    def add_node(self, node_config:dict):
         """Handle the add node event but activating transfer and returning device info"""
-        with open("device_list.json", "r") as f:
-            data = json.load(f)
-            hn = data[d_name]["hostname"]
-            un = data[d_name]["user"]
+        un = node_config["user"]
+        hn = node_config["hostname"]
         os_info = subprocess.run([script,un,hn],check=True,
                                  capture_output=True,text=True)
-        data[d_name]["operating_system"] = os_info.stdout.strip()
-        with open("device_list.json", "w") as f:
-            json.dump(data, f, indent=4)
+        node_config["operating_system"] = os_info.stdout.strip()
+       #with open("device_list.json", "w") as f:
+            #son.dump(data, f, indent=4)
 
         
 

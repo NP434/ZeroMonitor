040c0c11805467d8d2096d0a4c81faaae267bb8e
Grant Valentine <gw_valentine@yahoo.com>
2026-04-23 22:30:38 -0700

    Update test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import logging
 
 # --- LOGGING SET UP (MUST BE FIRST) ---
 # Create the hidden logs folder if it doesn't exist
-# Updoot Test #2
+# Updoot Test #3
 os.makedirs("logs", exist_ok=True)
 
 # Configure the global logging rules
